96adc8605f515337ac353d267977f1095f7ca997
artjom-spessivtsev <artjom.spesivcev@gmail.com>
2026-01-07 13:40:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace animalLength
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string[] animalName = { "Koer", "Kass", "Jänes", "Sipelgasiil", "Lendorav" };
+
+            string longestName = FindLongestAnimal(animalName);
+
+            Console.WriteLine(longestName);
+        }
+
+        static string FindLongestAnimal(string[] animals)
+        {
+            string longest = animals[0];
+
+            foreach (string animal in animals)
+            {
+                if (animal.Length > longest.Length)
+                {
+                    longest = animal;
+                }
+            }
+            Console.WriteLine(longest + " " + longest.Length);
+
+            return longest;
+        }
+    }
+}
